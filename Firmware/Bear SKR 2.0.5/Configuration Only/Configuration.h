ea8cb9df3e557e0397c44d620a254c64ce3e60a9
Narasak Mansurang <narasak.man@gmail.com>
2020-03-18 23:17:39 +0700

    Add ready to use firmware.bin

--- Firmware/Bear SKR 2.0.5/Configuration Only/Configuration.h
+++ Firmware/Bear SKR 2.0.5/Configuration Only/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Chris Warkocki, SKR V1.4 Bear BMG/TMC2209/PINDA)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Narasak Mansurang, Bear SKR V1)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "SKR Bear BMG Turbo"
+#define CUSTOM_MACHINE_NAME "Read Bear SKR"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
