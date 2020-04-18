c71d435c395fd29921ad1afba9d89f819414a5dc
Narasak Mansurang <narasak.man@gmail.com>
2020-04-18 11:02:28 +0700

    Update motor current for 0.9 Degree Stepper

--- Firmware/Bear SKR PINDA BMG 0.9 ALL Axis 2.0.5.3/Full Source Code/Marlin/Configuration_adv.h
+++ Firmware/Bear SKR PINDA BMG 0.9 ALL Axis 2.0.5.3/Full Source Code/Marlin/Configuration_adv.h
@@ -2034,7 +2034,7 @@
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       1100        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       1400        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  300  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     32    // 0..256
     #define X_RSENSE          0.11
@@ -2050,7 +2050,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       1300
+    #define Y_CURRENT       1400
     #define Y_CURRENT_HOME  400
     #define Y_MICROSTEPS     32
     #define Y_RSENSE          0.11
@@ -2098,7 +2098,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      1100
+    #define E0_CURRENT      1400
     #define E0_MICROSTEPS    32
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
